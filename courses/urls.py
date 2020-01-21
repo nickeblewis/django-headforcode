fc6fe68ee22155ad11c75c8de646dc952b0c9a18
nicklewiskier <nick.lewis2@kier.co.uk>
2020-01-21 11:56:36 +0000

    progress so far up to end of home page part

--- courses/urls.py
+++ courses/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# helloworld_project/urls.py
+# pages_project/urls.py
 from django.contrib import admin
 from django.urls import path, include # new
 
